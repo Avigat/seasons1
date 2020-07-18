e0e128f85f852ad4430f4de740660e8b0ed54452
Avigat <67509362+Avigat@users.noreply.github.com>
2020-07-18 19:40:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function summer(){
+    alert("Hey it's summer!")
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005653/summer_jt8yiy.png')"
+document.body.style.backgroundSize="100%";
+document.body.style.backgroundRepeat="no-repeat";
+}
+function winter(){
+    alert("Hey it's winter!")
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005659/winter_kxcbyk.png')"
+document.body.style.backgroundSize="100%";
+document.body.style.backgroundRepeat="no-repeat";
+}
+function autumn(){
+    alert("Hey it's autumn!")
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005628/autumn_ywwom5.png')"
+document.body.style.backgroundSize="100%";
+document.body.style.backgroundRepeat="no-repeat";
+}
+function spring(){
+    alert("Hey it's spring!")
+    document.body.style.backgroundImage="url('https://res.cloudinary.com/pavie/image/upload/v1595005642/spring_qejh9v.png')"
+document.body.style.backgroundSize="100%";
+document.body.style.backgroundRepeat="no-repeat";
+}
+function copy(){
+    alert("Lol you can't copy")
+}
